c7f7a1544ae5931bd6f7f8d56a5098b435872743
Rawaha-mr11 <rawahamr11@gmail.com>
2025-11-22 17:12:53 +0500

    Updated 11/22/2025

--- src/data/PortfolioBgImages.js
+++
@@ -1,6 +0,0 @@
-import img1 from '../assets/e1.avif'
-import img2 from '../assets/e2.jpg'
-import img3 from '../assets/e3.jpg'
-import img4 from '../assets/e4.jpg'
-
-export const bgImages = [img1, img2, img3, img4]
